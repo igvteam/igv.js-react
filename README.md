a4075986d231dcb3cb2ada270c7bc0721cab56f1
jrobinso <933148+jrobinso@users.noreply.github.com>
2023-03-27 10:53:06 -0700

    Updated example supporting ES2020

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# igv-react
-Example project for using igv.js with React
+
+# igv.js in React
+
+This is a minimal example of integrating the [igv.js](https://github.com/igvteam/igv.js) genome browser with the 
+popular [React](https://reactjs.org/) framework for front-end web development.  The project was bootstrapped 
+with [Create React App](https://github.com/facebookincubator/create-react-app).  
+
+The example was originally created by @eweiz, and can be found here:  https://github.com/eweitz/igv.js-react.
+
+# Install
+```
+git clone https://github.com/jrobinso/igv.js-react
+cd igv.js-react
+npm install
+npm start
+```
+
+# Output
+After executing the steps above, you should see the following:
+![igv.js in React screenshot](https://raw.githubusercontent.com/eweitz/igv.js-react/master/igv-js_react_example.png)
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { createRef, Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import igv from '../node_modules/igv/dist/igv.esm.js';
+
+var igvStyle = {
+  paddingTop: '10px',
+  paddingBottom: '10px',
+  margin: '8px',
+  border: '1px solid lightgray'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to igv.js in React!</h1>
+        </header>
+        <AppIgv/>
+      </div>
+    );
+  }
+}
+
+class AppIgv extends Component {
+
+  constructor(props) {
+    super(props)
+    // Declare reference object for the dom node that will contain the igv.js component
+    this.container = createRef(null)
+  }
+
+
+  componentDidMount() {
+    // Create the igv.js component.
+    const igvOptions = {genome: 'hg38', locus: 'BRCA1'};
+    return igv.createBrowser(this.container.current, igvOptions);
+  }
+
+  render() {
+    // Return the div that will contain igv.js
+    return (
+      <div ref={this.container}  style={igvStyle}></div>
+    );
+  }
+}
+
+
+export default App;
